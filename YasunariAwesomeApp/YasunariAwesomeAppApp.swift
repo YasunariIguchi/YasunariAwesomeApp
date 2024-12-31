73cea2dc31fe67f2514ead8e2304ee1eda658fa1
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2024-12-31 15:57:00 +0900

    Initial Commit

---
+++ YasunariAwesomeApp/YasunariAwesomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YasunariAwesomeAppApp.swift
+//  YasunariAwesomeApp
+//
+//  Created by 井口泰成 on 2024/12/31.
+//
+
+import SwiftUI
+
+@main
+struct YasunariAwesomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YasunariAwesomeAppTests/YasunariAwesomeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  YasunariAwesomeAppTests.swift
+//  YasunariAwesomeAppTests
+//
+//  Created by 井口泰成 on 2024/12/31.
+//
+
+import Testing
+@testable import YasunariAwesomeApp
+
+struct YasunariAwesomeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
